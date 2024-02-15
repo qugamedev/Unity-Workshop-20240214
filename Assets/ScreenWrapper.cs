4f5f7f0898007f20d0c6327c69c07f6b4e1e9efe
BluBloos <cnoah1705@gmail.com>
2024-02-15 09:19:05 -0500

    update noob code.

--- Assets/ScreenWrapper.cs
+++ Assets/ScreenWrapper.cs
@@ -8,31 +8,23 @@ public class ScreenWrapper : MonoBehaviour
 
     void Update()
     {
-       Vector2 dim = Game.GetScreenDim();
-       float screenWidth = dim.x + margin;
-       float screenHeight = dim.y + margin;
+        Vector2 dim = Game.GetScreenDim();
+        float screenWidth = dim.x + margin;
+        float screenHeight = dim.y + margin;
 
         Vector2 newPosition = transform.position;
 
         if (transform.position.x > screenWidth / 2)
-        {
             newPosition.x = -screenWidth / 2;
-        }
 
         if (transform.position.x < -screenWidth / 2)
-        {
             newPosition.x = screenWidth / 2;
-        }
 
         if (transform.position.y > screenHeight / 2)
-        {
             newPosition.y = -screenHeight / 2;
-        }
 
         if (transform.position.y < -screenHeight / 2)
-        {
             newPosition.y = screenHeight / 2;
-        }
 
         transform.position = newPosition;
     }
